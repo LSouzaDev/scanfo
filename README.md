6d8dfd101e096a68a2d0d74ee2fb5700366efde6
LSouzaDev <40007618+LSouzaDev@users.noreply.github.com>
2021-04-21 00:57:38 -0300

    Upload version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # scanfo
-Scanner for Unix Systems
+Scanner Forensic for Unix Systems
+ 
+Um scanner forense para sistemas unix para analises de sistemas utilizando um script em bash.
+
+## Captura de Tela
+![Captura de Tela do programa em execução](./printscreen.png "Captura de Tela")
+
+## Utilização
+`./scanfo.sh`
+
+Após a execução um arquivo ***info.txt*** será gerado.
+
+## Que tipo de dado é coletado?
+* Qual Sistema está sendo Utilizado
+* Versão do Kernel
+* Arquitetura do Sistema
+* Nome de Usuário
+* Localização da Pasta do Usuário
+* Endereço IP Externo
+* Dispositivos Conectados
+* Usuários Conectados
+* Conexões de rede
+* Lista de Arquivos Abertos
+* Processos do Sistema
+* Interfaces de rede (ifconfig)
+* Registro dos Ultimos Logins
+* /etc/passwd
+* /etc/groups
+* /etc/shadow (root requerido)
+* crontab (root/root requerido)
+* crontab (usuario)
+* Espaço em disco
+* Quantidade de Memória RAM
+* Binários SUID/SGID
+* Verifica RootKits no Sistema
+
+
+### **ATENÇÃO**
+Esse script foi testado no Zorin OS 15.3
---
+++ scanfo.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Script para coleta de informações do sistema
+#
+# Autor: Luiz Filipe Souza (lsouzadev)
+# Versão: 1.0
+# Github: https://github.com/lsouzadev/scanfo
+#
+# LICENSE: MIT
+#
+filename="sysinfo.txt"
+echo -e "\n███████╗ ██████╗ █████╗ ███╗   ██╗███████╗ ██████╗ "
+echo -e "██╔════╝██╔════╝██╔══██╗████╗  ██║██╔════╝██╔═══██╗"
+echo -e "███████╗██║     ███████║██╔██╗ ██║█████╗  ██║   ██║"
+echo -e "╚════██║██║     ██╔══██║██║╚██╗██║██╔══╝  ██║   ██║"
+echo -e "███████║╚██████╗██║  ██║██║ ╚████║██║     ╚██████╔╝"
+echo -e "╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝ "
+echo -e "=================== DADOS DO SISTEMA ALVO ===================" | tee -a $filename
+date "+[+] Data e Hora da Coleta: %d/%m/%Y %H:%M" | tee -a $filename
+echo -e "[+] Sistema: $(uname)" | tee -a $filename
+echo -e "[+] Kernel: $(uname -r)" | tee -a $filename
+echo -e "[+] Arquitetura: $(uname -m)" | tee -a $filename
+echo -e "[+] Usuário (Nome): $(echo $USER)" | tee -a $filename
+echo -e "[+] Usuário (ID): $(echo id -u)" | tee -a $filename
+echo -e "[+] Usuário (Pasta): $(echo $HOME)" | tee -a $filename
+echo -e "[+] Endereço IP (Externo/curl/ifconfig.me) : $(curl ifconfig.me)" | tee -a $filename
+echo -e "=================== DISPOSITIVOS CONECTADOS ===================" | tee -a $filename
+lsusb | tee -a $filename
+echo -e "=================== USUARIOS CONECTADOS ===================" | tee -a $filename
+w
+echo -e "=================== Conexões de Rede ===================" | tee -a $filename
+netstat -antup | tee -a $filename
+echo -e "=================== Arquivos Abertos ===================" | tee -a $filename
+lsof | tee -a $filename
+echo -e "=================== Processos do Sistema ===================" | tee -a $filename
+ps -elyLF | tee -a $filename
+echo -e "=================== Interfaces de Rede ===================" | tee -a $filename
+ifconfig -a -v
+echo -e "=================== Ultimos Logins ===================" | tee -a $filename
+last -adix | tee -a $filename
+echo -e "=================== /etc/passwd ===================" | tee -a $filename
+cat /etc/passwd | tee -a $filename
+echo -e "=================== /etc/groups ===================" | tee -a $filename
+cat /etc/groups | tee -a $filename
+echo -e "=================== /etc/shadow ===================" | tee -a $filename
+cat /etc/shadow | tee -a $filename
+echo -e "=================== Crontab ROOT ===================" | tee -a $filename
+crontab -u root -l | tee -a $filename
+echo -e "=================== Crontab USER ===================" | tee -a $filename
+crontab -l | tee -a $filename
+echo -e "=================== Espaço em disco ===================" | tee -a $filename
+df -h | tee -a $filename
+echo -e "=================== Memória RAM ===================" | tee -a $filename
+free -h | tee -a $filename
+echo -e "=================== Sistema de Arquivos ===================" | tee -a $filename
+mount | tee -a $filename
+echo -e "=================== Binários SUID/SGID ===================" | tee -a $filename
+find / -perm -004000 -o -perm -002000 2>/dev/null | tee -a $filename
+echo -e "=================== RootKits? (chkrootkit) ===================" | tee -a $filename
+chkrootkit | tee -a $filename
+echo -e "=================== Finalizado em $(date '+%d/%m/%Y %H:%M') ===================" | tee -a $filename
+
+clear
+cat $filename
